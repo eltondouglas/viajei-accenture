8df6e64b1662303b2548e69c649b00d7dcef0377
JefersonSilva <jefersoun.silva@gmail.com>
2020-10-30 10:25:23 -0300

    first commit

---
+++ src/main/java/com/accenture/gama/viajei/ViajeiApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.gama.viajei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajeiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/gama/viajei/ViajeiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.gama.viajei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViajeiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
