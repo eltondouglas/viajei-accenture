3e3f01178c6392433aaab62c859dd919cf8f5a04
Jeferson Silva <jefersoun.silva@gmail.com>
2020-10-30 12:43:24 -0300

    Create FlightOfferSearchService

---
+++ src/main/java/com/accenture/gama/viajei/service/FlightOfferSearchService.java
@@ -0,0 +1,30 @@
+package com.accenture.gama.viajei.service;
+
+import java.time.LocalDate;
+
+import com.amadeus.Amadeus;
+import com.amadeus.Params;
+import com.amadeus.exceptions.ResponseException;
+import com.amadeus.resources.FlightOfferSearch;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FlightOfferSearchService {
+    @Autowired
+    private Amadeus amadeus;
+
+    public FlightOfferSearch[] getFlightOffersPrice(String originLocationCode, String destinationLocationCode, LocalDate departureDate, LocalDate returnDate, int adults) {
+        try {
+            FlightOfferSearch[] flightOfferSearchs = amadeus.shopping.flightOffersSearch
+                    .get(Params.with("originLocationCode", originLocationCode).and("destinationLocationCode", destinationLocationCode)
+                            .and("departureDate", departureDate).and("returnDate", returnDate).and("adults", adults).and("children", 3).and("max", 1));
+            return flightOfferSearchs;
+        } catch (ResponseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
