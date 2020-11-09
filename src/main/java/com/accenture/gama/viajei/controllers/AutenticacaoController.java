bac264f48fe387f2a46a69b8182e3dfde0cff27e
Elton Santos <eltondouglas@live.com>
2020-11-09 16:20:49 -0300

    Adicionado o response status created no cadastro de usuário

--- src/main/java/com/accenture/gama/viajei/controllers/AutenticacaoController.java
+++ src/main/java/com/accenture/gama/viajei/controllers/AutenticacaoController.java
@@ -1,6 +1,7 @@
 package com.accenture.gama.viajei.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -9,6 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.accenture.gama.viajei.model.dto.Credencial;
@@ -36,6 +38,7 @@ public class AutenticacaoController {
 	@Autowired
 	private UsuarioRepostiry userRepository;
 	
+	@ResponseStatus(code = HttpStatus.CREATED)
 	@PostMapping("/signin")
 	public void signin(@RequestBody ViajanteDTO viajante) {		
 		service.save(viajante.toViajante());
